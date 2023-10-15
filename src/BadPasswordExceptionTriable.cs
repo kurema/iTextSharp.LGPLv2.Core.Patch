1f0bb98e71aef3eb395709d74c74c8f85f15bfe0
kurema <kurema_makoto_software@yahoo.co.jp>
2023-10-15 10:00:35 +0900

    Init

---
+++ src/BadPasswordExceptionTriable.cs
@@ -0,0 +1,42 @@
+﻿namespace iTextSharp.text.pdf;
+
+public class BadPasswordExceptionTriable : BadPasswordException
+{
+	public BadPasswordExceptionTriable(Func<byte[], TryPasswordResult> tester) : base()
+	{
+		this.Tester = tester;
+	}
+	public BadPasswordExceptionTriable(string message, Func<byte[], TryPasswordResult> tester) : base(message)
+	{
+		this.Tester = tester;
+	}
+	public BadPasswordExceptionTriable(string message, Exception innerException, Func<byte[], TryPasswordResult> tester) : base(message, innerException)
+	{
+		this.Tester = tester;
+	}
+	public TryPasswordResult? TryPassword(byte[] password)
+	{
+		if (Tester is null) return null;
+		return Tester.Invoke(password);
+	}
+	public bool CanTryPassword => Tester is not null;
+	internal Func<byte[], TryPasswordResult> Tester { get; }
+	public enum TryPasswordResult
+	{
+		SuccessOwnerPassword, SuccessUserPassword, Fail,
+	}
+
+	public static bool IsSucess(TryPasswordResult? arg) => arg switch { TryPasswordResult.SuccessOwnerPassword or TryPasswordResult.SuccessUserPassword => true, _ => false };
+
+	internal BadPasswordExceptionTriable()
+	{
+	}
+
+	internal BadPasswordExceptionTriable(string message) : base(message)
+	{
+	}
+
+	internal BadPasswordExceptionTriable(string message, Exception innerException) : base(message, innerException)
+	{
+	}
+}
